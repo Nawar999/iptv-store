f15e5d8272180ee631aa79d704aa6d8f5d46c229
Nawar999 <nawar533398@gmail.com>
2024-04-02 00:15:30 +0300

    first

---
+++ app/components/MainPage.jsx
@@ -0,0 +1,13 @@
+// import Header from "./Header";
+// import Footer from "./Footer";
+import Hero from "./Hero";
+import Products from "./Products";
+
+export default function MainPage() {
+  return (
+    <div>
+      <Hero />
+      <Products />
+    </div>
+  );
+}
